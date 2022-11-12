fd53240fc9ee584dc6bc9c65c5faaecf91240450
Ayush Gandhi <agandhi62@gmail.com>
2022-11-12 16:18:39 +0530

    initial commint

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Home page</h1>
+      <p>Some Contet</p>
+      <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        Tailwind button
+      </button>
+    </div>
+  );
+}
